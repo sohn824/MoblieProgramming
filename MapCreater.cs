6afc5e52c6c220a9727f23f24a7cff73358bb0a1
sohn824 <37131599+sohn824@users.noreply.github.com>
2018-10-10 17:26:02 +0900

    Update MapCreater.cs

---
+++ MapCreater.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreater : MonoBehaviour
+{
+    [SerializeField]
+    private int width = 12;
+    [SerializeField]
+    private int height = 12;
+    [SerializeField]
+    private GameObject tile;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private GameObject enemy;
+
+    private int randomX;
+    private int randomZ;
+	// Use this for initialization
+	void Start ()
+    {
+        for(int i=0; i<width; i++)
+        {
+            for(int j=0; j<height; j++)
+            {
+                Instantiate(tile, new Vector3(i, 0, j), Quaternion.identity, GameObject.Find("Map").transform);
+            }
+        }
+        setPlayer();
+	}
+	
+    private void setPlayer()
+    {
+        Instantiate(player, new Vector3(Random.Range(0, width), 1.5f, Random.Range(0, height)), Quaternion.identity);
+        Instantiate(enemy, new Vector3(Random.Range(0, width), 1.5f, Random.Range(0, height)), Quaternion.identity);
+    }
+}
